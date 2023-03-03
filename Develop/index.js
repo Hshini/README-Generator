03f7a5942bb33d785ad6746413159e8219cc0504
Shini <halimshini@hotmail.com>
2023-03-02 23:48:38 -0500

    initialc commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,39 @@
-// TODO: Include packages needed for this application
-const inquirer=require
+// Dependencies
+var inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:input,
+        message:"What is your Name?",
+        name:userName
+    },
+    {
+        type:input,
+        message:"What is your Name?",
+        name:userName
+    },
+    {
+        type:input,
+        message:"What is your Name?",
+        name:userName
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('text.txt', content, err => {
+        if (err) {
+          console.error(err);
+        }
+        
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
