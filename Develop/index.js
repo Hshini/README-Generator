3014ded9ed6fa9c56d1f8f931003c264a9eafdb0
Shini <halimshini@hotmail.com>
2023-03-11 23:30:30 -0500

    added  function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,86 +1,84 @@
 // Dependencies
 var inquirer = require('inquirer');
-const markDown = require ("./utils/generateMarkdown")
+const markDown = require("./utils/generateMarkdown")
 const fs = require('fs');
-console.log(markDown)
-// const markDown = require ('generateMarkdown.js');
-// const { title } = require('process');
-// TODO: Create an array of questions for user input
+
+const { title } = require('process');
+//Array of questions for user input
 const questions = [
-   
+
     {
-        type:"input",
-        message:"What is the title of project?",
-        name:"title"
+        type: "input",
+        message: "What is the title of project?",
+        name: "title"
     },
     {
-        type:"input",
-        message:"Project Description?",
-        name:"description"
+        type: "input",
+        message: "Enter Project Description?",
+        name: "description"
     },
     {
-        type:"input",
-        message:"Installation Instrucition?",
-        name:"instruction"
+        type: "input",
+        message: "Enter Installation Instrucition?",
+        name: "instruction"
     },
     {
-        type:"input",
-        message:"Usage information?",
-        name:"usage"
+        type: "input",
+        message: "Enter Usage information?",
+        name: "usage"
     },
     {
-        type:"input",
-        message:"Contributing",
-        name:"contributing"
+        type: "input",
+        message: "How  others can Contribute?",
+        name: "contributing"
     },
 
     {
-        type:"input",
-        message:"Installation Instrucition?",
-        name:"test"
+        type: "input",
+        message: "Enter Installation Instrucition?",
+        name: "instrucition"
     },
     {
-        type:"list",
-        message:"License Type?",
-        name:"license",
-        choices:['Academic Free License v3.0','MIT','ISC','The Unlicense']
+        type: "list",
+        message: "License Type?",
+        name: "license",
+        choices: ['Academic Free License v3.0', 'MIT', 'The Unlicense']
 
     },
     {
-        type:"input",
-        message:"Github Username?",
-        name:"userName"
+        type: "input",
+        message: "what is your Github Username?",
+        name: "userName"
     },
     {
-        type:"input",
-        message:"Email",
-        name:"email"
+        type: "input",
+        message: "what is your Email Addres",
+        name: "email"
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    //filename = reademe.md / index.html
-    fs.writeFile(fileName, data,'utf-8', err => {
+    fs.writeFile(fileName, data, 'utf-8', err => {
         if (err) {
-          console.error(err);
+            console.error(err);
         }
-        console.log("your file has been created")
-      });
+        console.log("Your file has been created")
+    });
 }
 
-// TODO: Create a function to initialize app
+
 //User Interaction
 
 function init() {
     inquirer.prompt(questions)
-    .then((response) =>{
-        console.log(response)
-        let templateData = markDown(response)
-        console.log(templateData)
-        writeToFile("readme.md", templateData);
-    }   
-    )
+        .then((response) => {
+            console.log(response)
+            let templateData = markDown(response)
+            console.log(templateData)
+            writeToFile("readme.md", templateData);
+        }
+        )
 
 }
 
--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # README-Generator
 
 ## Description
-
+This app will help anyone who wants to create professional README file.
 ## Installation 
+This generator requires node.js, npm
+In order to install Inquirer, please use npm i inquirer@8.2.4.
 
 ## Usage 
-
-## Contribution 
+Clone the repo you your pc.
+README-Generator will ask you a set of questions, which will  fill out the README.md
 
 ## License
 MIT
+
+## Questions
+halimshini@hotmail.com
